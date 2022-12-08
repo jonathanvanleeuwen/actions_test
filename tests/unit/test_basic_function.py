32131b599ed6afa54829a058db583d5dcf8275b0
Jonathan <jonathan89j@hotmail.com>
2022-12-08 18:38:39 +0100

    fix: test

--- tests/unit/test_basic_function.py
+++ tests/unit/test_basic_function.py
@@ -7,7 +7,6 @@ from actions_test.basic_function import add, subtract
     [
         pytest.param(1, 2, 3, id="correct 1"),
         pytest.param(0, 2, 2, id="correct 2"),
-        pytest.param(1, 2, 4, id="incorrect throw error"),
     ],
 )
 def test_add(a, b, expected):
